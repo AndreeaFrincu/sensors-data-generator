37e2901abd3a559610cb48b35d996fa208eacc98
Andreea Frincu <frincu60@gmail.com>
2021-06-30 16:46:25 +0300

    hard-coded endpoint

--- post_data.py
+++ post_data.py
@@ -3,11 +3,10 @@ import urllib
 import generator
 import requests
 import json
-import os
 
 def post_sensor_data():
     payload = generator.gen_sensor_metric()
     # print(payload)
     dump = json.dumps(payload)
     data = json.loads(dump)
-    requests.post(url=os.environ['ENDPOINT'], json=data)
+    requests.post(url="http://192.168.0.102:8080/api/metrics", json=data)
