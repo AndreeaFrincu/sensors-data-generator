67e8e694d7bd3a433df7f149a6a6610e899dff07
Andreea Frincu <frincu60@gmail.com>
2021-06-30 16:23:45 +0300

    modify metricType from uppercase to lowercase

--- post_data.py
+++ post_data.py
@@ -1,8 +1,13 @@
+import urllib
+
 import generator
 import requests
+import json
 import os
 
 def post_sensor_data():
     payload = generator.gen_sensor_metric()
     # print(payload)
-    requests.post(url=os.environ['ENDPOINT'], json=payload)
+    dump = json.dumps(payload)
+    data = json.loads(dump)
+    requests.post(url=os.environ['ENDPOINT'], json=data)
